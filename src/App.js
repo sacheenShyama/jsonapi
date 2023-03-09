be32db43eb60e8d1ef11c5c087131b52c14151f8
sacheenShyama <nomadsachin2002@gmail.com>
2023-03-09 16:20:30 +0530

    setup

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      hello
+      {/* <div className="header"></div>
+      <div className="msgContent">
+        <div></div>
+        <div></div>
+      </div>
+      <div className="sender">
+        <div>
+          <input />
+        </div>
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
